970d5cff77b5756087ca71df5498472fdb053a68
Ivan Kravarščan <subchannel13@gmail.com>
2024-04-24 21:42:14 +0200

    Assignment service tests

--- frontend/src/app/data/assignment.service.spec.ts
+++ frontend/src/app/data/assignment.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import {
+    HttpClientTestingModule,
+    HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { AssignmentService } from './assignment.service';
+import { Assignment } from './assignment.model';
+
+const mockEntity = new Assignment(1, "Test task");
 
 describe('AssignmentService', () => {
-  let service: AssignmentService;
+    let service: AssignmentService;
+    let httpController: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AssignmentService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+        });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+        service = TestBed.inject(AssignmentService);
+        httpController = TestBed.inject(HttpTestingController);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should list assignments', () => {
+        service.list().subscribe((result) => {
+            expect(result).toEqual([mockEntity]);
+        });
+        
+        const req = httpController.expectOne({
+            method: 'GET',
+            url: '/api/Assignment/List',
+        });
+
+        req.flush([mockEntity]);
+        httpController.verify();
+    });
 });
